c4b79f6c4ed806461aea042b689c99b669f9c2c5
jihye <zehye.01@gmail.com>
2019-11-03 16:46:45 +0900

    first commit

---
+++ app/config/settings/local.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': get_env_variable('LOCAL_DB_ENGINE'),
+        'NAME': get_env_variable('LOCAL_DB_NAME'),
+        'USER': get_env_variable('LOCAL_DB_USER'),
+        'PASSWORD': get_env_variable('LOCAL_DB_PASSWORD'),
+        'HOST': get_env_variable('LOCAL_DB_HOST'),
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
+    }
+}
+
+
